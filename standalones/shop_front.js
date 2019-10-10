f92b2da786d2555d18ef2bf13b9aa0472522079e
Lucian LaFleur <johnksummerlin@hotmail.com>
2019-10-10 20:26:11 +0200

    dummy validation form styled with better coloring. Gave colored outline to submitBtn if all validation switches are true. Commented out samples from the 'cart' in HTML.

--- standalones/shop_front.js
+++ standalones/shop_front.js
@@ -75,23 +75,46 @@
 
 
 // ********************
+// instantiate switch booleans for checking if forms are valid
+let goodName = false;
+let goodZip = false;
+let goodEMail = false;
+let goodPhone = false;
 
-// blur event listeners for the form inputs
+// when item loses focus, run function on the element to check validation
 document.getElementById('name').addEventListener('blur', validateName);
 document.getElementById('zip').addEventListener('blur', validateZip);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
+const submitBtn = document.getElementById('submit-btn')
+// check for all validations when hovering over submit
+submitBtn.addEventListener('click', validateAll);
+
+function submitStyleControl() {
+  if(goodName && goodZip && goodEMail && goodPhone){
+    submitBtn.style.outlineColor = "lime";
+    submitBtn.style.background = "#37f";
+  } else {
+    submitBtn.style.outlineColor = "#666";
+    submitBtn.style.background = "#225";
+  }
+}
+
 // use regex to validate names
 function validateName() {
   const name = document.getElementById('name');
   // 3-10 letters from a-z
-  const re = /^[a-zA-z]{3,10}$/;
+  const re = /^[a-zA-Z]{3,10}$/;
 
   if(!re.test(name.value)) {
     name.classList.add('is-invalid');
+    goodName = false;
+    submitStyleControl() 
   } else {
     name.classList.remove('is-invalid');
+    goodName = true;
+    submitStyleControl();
   }
 }
 
@@ -103,18 +126,26 @@ function validateZip() {
 
   if(!re.test(zip.value)) {
     zip.classList.add('is-invalid');
+    goodZip = false;
+    sumbitStyleControl();
   } else {
     zip.classList.remove('is-invalid');
+    goodZip = true;
+    submitStyleControl(); 
   }
 }
 
 function validateEmail() {
   const email = document.getElementById('email');
-  const re = /^([a-zAZ0-9_\-\.]+)@([a-zAZ0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
   if(!re.test(email.value)) {
     email.classList.add('is-invalid');
+    goodEMail = false;
+    submitStyleControl(); 
   } else {
     email.classList.remove('is-invalid');
+    goodEMail = true;
+    submitStyleControl(); 
   }
 }
 
@@ -124,7 +155,21 @@ function validatePhone() {
   const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
   if(!re.test(phone.value)) {
     phone.classList.add('is-invalid');
+    goodPhone = false;
+    submitStyleControl(); 
   } else {
     phone.classList.remove('is-invalid');
+    goodPhone = true;
+    submitStyleControl(); 
   }
 }
+
+function validateAll(){
+  if(goodName && goodZip && goodEMail && goodPhone){
+    alert('All fields are valid, thanks for partaking in this form demo');
+  } else {
+    alert('You appear to have some missing or invalid fields');
+  }
+}
+
+//  set a timeout to make the newly purchased item be hilit with a bright cyan background for a few seconds at first
