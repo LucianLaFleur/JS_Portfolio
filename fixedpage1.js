8b2ed8d7b6c6f0461bc46d63910d8729f6c02e7c
Lucian LaFleur <johnksummerlin@hotmail.com>
2019-10-16 23:20:55 +0200

    box opening clicker finished, all button disables/enables work fine. Added confirm message for Crate Reset button

--- fixedpage1.js
+++ fixedpage1.js
@@ -1,9 +1,123 @@
+// progress bar manipulaitons
+let barPercentFull = 0;
+let boxOpen = false;
+//  rate at which box opens
+let pryPerClick = 25;
+let coins = 0;
+
+// INC: different tiers of boxes randomly appear?
+let intactBoxImg = document.getElementById("intactBoxImg");
+let openBoxImg = document.getElementById('openBoxImg');
+let pryBtn = document.getElementById('pryBtn');
+let rewardBtn = document.getElementById('rewardBtn');
+// disable reward btn because it starts off un-clickable
+rewardBtn.disabled = true;
+
+function showHideRewardBtn() {
+  if (boxOpen) {
+    intactBoxImg.style.display = "none"
+    openBoxImg.style.display = "block"
+    rewardBtn.disabled = false;
+    rewardBtn.innerHTML = `Collect loot!`;
+  } else {
+    // if box isn't open, show intact box and keep open box hidden
+    intactBoxImg.style.display = "block"
+    openBoxImg.style.display = "none"
+    rewardBtn.disabled = true;
+    rewardBtn.innerHTML = 'crate not broken';
+  }
+}
+
+function showBarPercent(){
+  inbar1.style = "width:" + barPercentFull + "%;"
+  breakPercent.innerHTML = ` ${barPercentFull} %`;
+}
+
+function increasePryPercent() {
+    // variable call for box-open rate per-click
+  barPercentFull += pryPerClick;
+  // add the value to the inner bar
+  showBarPercent();
+  if(barPercentFull >= 100){
+    boxOpen = true;
+    pryBtn.style.display = 'none';
+    // play sound for opening box
+    inbar1.style.background = "gold";
+    breakPercent.innerHTML = ` 100 %`;
+    showHideRewardBtn()
+  }
+}
+
+function getPaid(){
+  // re-do so coins are worth the value of a fish extracted from a randomized array
+  boxOpen = false;
+  // change btn text after loot is gathered
+  rewardBtn.innerHTML = 'crate looted';
+  rewardBtn.disabled = true;
+  barPercentFull = 0;
+  coinsFound = Math.ceil((Math.random() * 15) + 5);
+  console.log (`Found ${coinsFound} coins!`)
+  // add coins to pouch
+  coins += coinsFound;
+  coinPouch.innerHTML = coins;
+};
+
+// function clearBank(){
+//   let dataMsg;
+//   if (confirm("Are you sure you want to clear all points earned?")) {
+//       window.localStorage.clear();
+//       dataMsg = "Okay, deleting all "
+//       console.log(dataMsg + this.gameController.pts + " pts");
+//   } else {
+//       dataMsg = "aborted deletion process"
+//       console.log(dataMsg);
+//   }
+// }
+
+function resetCrate() {
+  boxOpen = false;
+  rewardBtn.disabled = true;
+  barPercentFull = 0;
+  showBarPercent();
+  rewardBtn.innerHTML = 'crate not broken';
+  pryBtn.style.display = 'inline-block';
+  showHideRewardBtn();
+}
+
+function crateResetWarning(){
+  if(boxOpen){
+    if (confirm("Are zombies so close that you want to abandon an opened crate?")) {
+        warnMsg = "You abandoned the loot"
+        console.log(warnMsg);
+        resetCrate();
+    } else {
+        warnMsg = "You decided not to abandon the loot"
+        console.log(warnMsg);
+        // do not reset open crate if they pick cancel
+    }
+  } else if (barPercentFull > 0) {
+    if (confirm(`Abandon progress?`)) {
+      warnMsg = "You abandoned the loot"
+      console.log(warnMsg);
+      resetCrate();
+  } else {
+      warnMsg = "You decided not to abandon the loot"
+      console.log(warnMsg);
+      // do not reset open crate if they pick cancel
+  }
+  }
+}
+
 // this.setinterval? NEED RESEARCH INTERVAL AND CLEAR INTERVAL!!!
 // setInterval, clear interval will stop the thing from running
 var d, h, m, s, hdeg, ampm;
 
 // no need for document.getelementbyid?
 // INC: make the clock chime different tunes on the 30 and hr?
+
+function setThirtyM() {
+  document.getElementById('timer-length').value = 1800;
+}
 
 function adjustClock(){
   d = new Date();
