31ec11e150764df29cad2d434ec650a6e2f662cd
Lucian LaFleur <johnksummerlin@hotmail.com>
2019-10-20 19:22:30 +0200

    updated angularapp following the traversy media course lesson 5-4, beginning on two-way data binding

--- angularProjects/angularapp/src/app/app.module.ts
+++ angularProjects/angularapp/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/Forms';
+
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ModelsComponent } from './components/models/models.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ModelsComponent
+    UserComponent,
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
