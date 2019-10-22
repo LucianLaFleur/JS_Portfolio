21d5625e7b6cb23ae410189319c70e3cb7245b0e
Lucian LaFleur <johnksummerlin@hotmail.com>
2019-10-23 01:25:48 +0200

    functions added to randomly deal out cards from the deck array and to visually show the cards (nums and syms, unstyled)

--- standalones/warCards/warCards.js
+++ standalones/warCards/warCards.js
@@ -17,7 +17,13 @@ var cardMaster = new Vue ({
     // userPlay: document.querySelector(".userPlay"),
     // res: document.querySelector(".res"),
     deck: [],
+    // placeholder arr for each player in game
+    players: [],
+    deals: [],
+    // temp holder gets first used in makeCards()
+    tempHolder: [],
     ranks: [2,3,4,5,6,7,8,9,10,"J","Q","K","A"],
+    // !!! Note that these match the symbol patterns, which we'll use later
     suits: ["hearts", "diams", "clubs", "spades"]
   },
   methods: {
@@ -32,7 +38,7 @@ var cardMaster = new Vue ({
         this.startGame();
     },
     buildDeck: function(){
-      this.deck=[];
+      let deck = this.deck;
       // ?????? suits.forEach ?????
       // for each suit
       for(let i=0; i<this.suits.length; i++){
@@ -42,13 +48,14 @@ var cardMaster = new Vue ({
           card.suit = this.suits[i];
           card.rank = this.ranks [j];
           card.value = (j + 1);
-          this.deck.push(card);
+          deck.push(card);
         }
       }
     },
     setupPlayers: function(num){
-      players= [];
-      deals= [];
+      // mutate the data object containing players in an array
+      players= this.players;
+      deals= this.deals;
       for(let x=0; x<num; x++){
         let div = document.createElement("div");
         div.setAttribute("id","player"+(x+1));
@@ -63,10 +70,63 @@ var cardMaster = new Vue ({
         deals.push([]);
       }
     },
+    showCard(el, card){
+      if(card!=undefined){
+        console.log(el+ " and " + card);
+      // if(card) since we're checking for truthiness in JS
+      el.style.backgroundColor = "white";
+      // this card.suit splicing only works because the symbols for each match the strings we declared prior for each suit, following sym format.
+      let html1 = card.rank + "<br>&" + card.suit + ";";
+      let html2 = card.rank +"&" + card.suit + ";";
+      let div = document.createElement("div");
+      // NOTE: "card" alone interferes with bootstrap
+      div.classList.add("pCard");
+
+      // ??? Assuming these are top and bottom suit symbols?
+      let span1 = document.createElement("span");
+      span1.innerHTML = html2;
+      div.appendChild(span1);
+      
+      let span2 = document.createElement("span");
+      span2.innerHTML = html1;
+      div.appendChild(span2);
+
+      el.appendChild(div);
+      }
+    },
+    dealCards: function(playerCard){
+      let deck = this.deck;
+      playerCard = (playerCard >= players.length) ? 0: playerCard;
+      // INC: re-write this to make it a while/until style loop?
+      if(deck.length>0){
+        let randIndex = Math.floor(Math.random()*deck.length);
+        let card = deck.splice(randIndex, 1)[0];
+        this.deals[playerCard].push(card);
+        playerCard++;
+        return this.dealCards(playerCard);
+      }else{
+        MessageChannel.textContent= "all cards dealt"; 
+        return;
+      }
+    },
+    makeCards: function(){
+      let tempHolder = this.tempHolder;
+      // test alternate method iterators...
+      // this.players.forEach(() => {
+      // });
+      // for (let x of this.players.length)
+      for(let x=0; x < this.players.length; x++){
+        let card = this.deals[x].shift();
+        tempHolder.push(card);
+        this.showCard(this.players[x],card);
+      }
+    },
     startGame: function(){
       let numberPlayers = document.querySelector("input").value;
       this.buildDeck();
       this.setupPlayers(numberPlayers);
+      this.dealCards(0);
+      this.makeCards();
     }
   },
   // computed: {
@@ -76,3 +136,6 @@ var cardMaster = new Vue ({
 
 // Don't need to listen for button clicks when you hold the power of Vue in your hands...
 // this.cardMaster.listenForBtnClick();
+
+
+// made vue data object for [players] array, mutates responsive to user input value within setupPlayers() .
