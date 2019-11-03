f17a6f9341cb57326df01b2dc199354dd6dde3ac
Lucian LaFleur <johnksummerlin@hotmail.com>
2019-11-03 04:05:08 +0100

    Allows for ball to move a bit behind paddle range before score is made (max x and -x coords for [ ball ] increased, and collision conditionals adjusted)

--- python/pong.py
+++ python/pong.py
@@ -112,14 +112,14 @@ while True:
     # os.system("afplay bounce.wav&")
 
   # Left and right
-  if ball.xcor() > 350:
+  if ball.xcor() > 380:
     score_a += 1
     pen.clear()
     pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Helvetica", 24, "normal"))
     ball.goto(0, 0)
     ball.dx *= -1
 
-  elif ball.xcor() < -350:
+  elif ball.xcor() < -380:
     score_b += 1
     pen.clear()
     pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Helvetica", 24, "normal"))
@@ -127,11 +127,11 @@ while True:
     ball.dx *= -1
 
   # Paddle and ball collisions
-  if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+  if (ball.xcor() < -340 and ball.xcor < -350) and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
     ball.dx *= -1
       # os.system("afplay bounce.wav&")
 
-  elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+  elif (ball.xcor() < 340 and ball.xcor < 350)  and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
     ball.dx *= -1
       # os.system("afplay bounce.wav&")
 
